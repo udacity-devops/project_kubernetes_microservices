2f0a2c9fcd0d342d70ea4d8f657632bdbc5b21b3
eduardo <eduardo@logcomex.com>
2020-06-16 22:06:35 -0300

    Kubernetes set up to run locally with 1 pod using docker hub image

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=edunicastro/microservice_api
+podname=api-container-pod
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+# See some refs on https://github.com/udacity-devops/kubernetes_exercise_debug_pod/blob/master/step_by_step.txt
+kubectl run $podname --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward $podname 8000:80
